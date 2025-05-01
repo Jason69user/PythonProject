a05f51055fa7d27918ca1123abb0d6a8e498ff5c
Dimitry <dartahilles@mail.ru>
2025-05-01 20:19:53 +0400

    нажимаем на авторизацию

--- python_selenium.py
+++ python_selenium.py
@@ -13,10 +13,12 @@ base_url = 'https://www.saucedemo.com/' # Даём ссылку на тести�
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.maximize_window() # окно максимального разрешения
+
 user_name = driver_chrome.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input')
 user_name.send_keys("problem_user") # вводим логин
+
 password = driver_chrome.find_element(By.XPATH, '//*[@id="password"]')
 password.send_keys("secret_sauce") # вводим пароль
-time.sleep(10)
-driver_chrome.close()
 
+button_login = driver_chrome.find_element(By.ID, 'login-button')
+button_login.click() # кликаем на авторизацию
