6282897cb3d78ffddc558355d8d93ea01f21e7b4
Dimitry <dartahilles@mail.ru>
2025-05-01 22:02:35 +0400

    добавляем проверку на корректность загружаемой страницы

--- python_selenium.py
+++ python_selenium.py
@@ -16,9 +16,24 @@ driver_chrome.maximize_window() # окно максимального разре
 
 user_name = driver_chrome.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input')
 user_name.send_keys("problem_user") # вводим логин
+print('input login')
 
 password = driver_chrome.find_element(By.XPATH, '//*[@id="password"]')
 password.send_keys("secret_sauce") # вводим пароль
+print('input password')
 
 button_login = driver_chrome.find_element(By.ID, 'login-button')
 button_login.click() # кликаем на авторизацию
+print(driver_chrome.current_url)
+
+get_url = driver_chrome.current_url
+url = 'https://www.saucedemo.com/inventory.html'
+assert url == get_url # сравниваем корректность url
+print('URL корректен')
+
+text_products = driver_chrome.find_element(By.XPATH, "//span[@class='title']")
+print(text_products.text) # ищем уникальное значение для определения корректности страницы
+
+value_text_products = text_products.text
+assert value_text_products == 'Products' # сравниваем уникальное значение с корректным
+print('Заголовок корректен')
