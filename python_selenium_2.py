fb362a7cd4e9885b27896f25bfda46d706b5b3bf
Dimitry <dartahilles@mail.ru>
2025-05-06 18:55:45 +0400

    Ставим задержку на 3с и обновляем страницу

---
+++ python_selenium_2.py
@@ -0,0 +1,31 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/' # Даём ссылку на тестируемый сайт
+
+driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
+driver_chrome.maximize_window() # окно максимального разрешения
+
+user_name = driver_chrome.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input')
+user_name.send_keys("problem_user") # вводим логин
+print('input login')
+
+password = driver_chrome.find_element(By.XPATH, '//*[@id="password"]')
+password.send_keys("secret_sau") # вводим не корректный пароль
+print('input password')
+
+button_login = driver_chrome.find_element(By.ID, 'login-button')
+button_login.click() # кликаем на авторизацию
+print("Click Login")
+
+time.sleep(3)
+driver_chrome.refresh() # обновляем страницу
