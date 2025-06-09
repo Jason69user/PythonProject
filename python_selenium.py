d7596d0bfb5c01b58817decea484e7ad0b9d0028
Dimitry <dartahilles@mail.ru>
2025-06-09 19:54:23 +0400

    добавил перемещение вперед-назад в браузере

--- python_selenium.py
+++ python_selenium.py
@@ -20,16 +20,36 @@ user_name = driver_chrome.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/d
 user_name.send_keys("standard_user") # вводим логин
 print('input login')
 
+# вводим пароль
 password = driver_chrome.find_element(By.XPATH, '//*[@id="password"]')
-password.send_keys("secret_sauce") # вводим пароль
+password.send_keys("secret_sauce")
 print('input password')
 
+# кликаем на авторизацию
 button_login = driver_chrome.find_element(By.ID, 'login-button')
-button_login.click() # кликаем на авторизацию
+button_login.click()
 print("Click Login")
 
-menu = driver_chrome.find_element(By.ID, 'react-burger-menu-btn').click() # нажимаем сендвич меню
+# выбираем товар
+button_add_backpack = driver_chrome.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
+button_add_backpack.click()
+print("select product")
+
+# переходим в корзину
+button_cart_link = driver_chrome.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']")
+button_cart_link.click()
+print("select cart link")
+
 time.sleep(1)
-logout = driver_chrome.find_element(By.ID, "logout_sidebar_link").click() # нажмаем на разлогирование
+
+driver_chrome.back() # нажимаем "назад"
+print("select back")
+
+time.sleep(1)
+
+driver_chrome.forward() # нажимаем "вперед"
+print("select forward")
+
+time.sleep(1)
 
 driver_chrome.close()
