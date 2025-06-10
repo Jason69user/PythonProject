fc8bb9165bf01bd4ea4f425b0fc378fb09863a3c
Dimitry <dartahilles@mail.ru>
2025-06-10 10:14:41 +0400

    проверка чекбокса на странице

--- python_selenium.py
+++ python_selenium.py
@@ -1,4 +1,5 @@
 import time
+from tabnanny import check
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -11,45 +12,17 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://www.saucedemo.com/' # Даём ссылку на тестируемый сайт
+base_url = 'https://demoqa.com/checkbox' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
-driver_chrome.maximize_window() # окно максимального разрешения
+driver_chrome.set_window_size(1920, 1080) # окно разрешения
 
-user_name = driver_chrome.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input')
-user_name.send_keys("standard_user") # вводим логин
-print('input login')
+# кликаем на чекбокс "home"
+check_box = driver_chrome.find_element(By.XPATH, "//span[@class='rct-checkbox']")
+check_box.click()
+check_box.is_selected()
+print("checkbox select")
 
-# вводим пароль
-password = driver_chrome.find_element(By.XPATH, '//*[@id="password"]')
-password.send_keys("secret_sauce")
-print('input password')
-
-# кликаем на авторизацию
-button_login = driver_chrome.find_element(By.ID, 'login-button')
-button_login.click()
-print("Click Login")
-
-# выбираем товар
-button_add_backpack = driver_chrome.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
-button_add_backpack.click()
-print("select product")
-
-# переходим в корзину
-button_cart_link = driver_chrome.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']")
-button_cart_link.click()
-print("select cart link")
-
-time.sleep(1)
-
-driver_chrome.back() # нажимаем "назад"
-print("select back")
-
-time.sleep(1)
-
-driver_chrome.forward() # нажимаем "вперед"
-print("select forward")
-
-time.sleep(1)
+time.sleep(2)
 
 driver_chrome.close()
