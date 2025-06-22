4948338ced81767b5bb8a3dbc7fba9e7d62e0f1e
Dimitry <dartahilles@mail.ru>
2025-06-22 13:55:29 +0400

    проверка двойного и правой кнопки мыши

--- python_selenium.py
+++ python_selenium.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,20 +12,21 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://demoqa.com/radio-button' # Даём ссылку на тестируемый сайт
+base_url = 'https://demoqa.com/buttons' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.set_window_size(1920, 1080) # окно разрешения
+action = ActionChains(driver_chrome)
 
-# Кликаем по Impressive
-radio_button = driver_chrome.find_element(By.XPATH, '(//label[@class="custom-control-label"])[2]')
-radio_button.click()
+# имитируем двойной клик мыши по кнопке
+double_click = driver_chrome.find_element(By.XPATH, "//button[@id = 'doubleClickBtn']")
+action.double_click(double_click).perform()
+print('Двойной клик мыши')
 
-# делаем проверку через сравнение
-radio_selected = driver_chrome.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div[2]/p/span')
-radio_text = radio_selected.text
-assert radio_text == 'Impressive'
-print("radio button выбран")
+# имитируем клик правой кнопки мыши
+right_click = driver_chrome.find_element(By.XPATH, "//button[@id = 'rightClickBtn']")
+action.context_click(right_click).perform()
+print('Клик ПКМ')
 
 time.sleep(2)
 
