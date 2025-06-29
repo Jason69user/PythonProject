19fb9589d29cb6ece769f4e51dd8a877c95c5b9b
Dimitry <dartahilles@mail.ru>
2025-06-29 19:59:32 +0400

    двигаем ползунок Round с проверкой

--- python_selenium.py
+++ python_selenium.py
@@ -1,9 +1,8 @@
 import time
 
 from selenium import webdriver
-from datetime import datetime, timedelta
+from selenium.webdriver import ActionChains
 
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,24 +13,21 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://demoqa.com/date-picker' # Даём ссылку на тестируемый сайт
+base_url = 'https://html5css.ru/howto/howto_js_rangeslider.php' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.set_window_size(1920, 1080) # окно разрешения
+action = ActionChains(driver_chrome)
 
-# удаляем поле с датой
-data_input = driver_chrome.find_element(By.XPATH, '//*[@id="datePickerMonthYearInput"]')
-data_input.send_keys(Keys.CONTROL + 'a')
-data_input.send_keys(Keys.DELETE)
+# двигаем ползунок Round
+slider = driver_chrome.find_element(By.XPATH, '//input[@class = "slider-color"]')
+action.click_and_hold(slider).move_by_offset(-500, 0).release().perform()
+check_value = driver_chrome.find_element(By.XPATH, "//span[@id = 'f']")
+check_text = check_value.text
+print(f"Значение value: {check_text}")
+assert check_text == "11"
+print('Проверка пройдена')
 
-time.sleep(1)
-
-# вставляем дату на 10 дней вперед от сегодня
-days_later = datetime.now() + timedelta(days=10)
-current_data = days_later.strftime('%m.%d.%Y')
-print(current_data)
-data_input.send_keys(current_data)
-
-time.sleep(2)
+time.sleep(5)
 
 driver_chrome.close()
