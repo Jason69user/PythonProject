63d7ad2d92a9573ecf626d274ff796717308821a
Dimitry <dartahilles@mail.ru>
2025-06-30 08:40:17 +0400

    добавил комментарий

--- python_selenium.py
+++ python_selenium.py
@@ -16,18 +16,19 @@ driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDr
 base_url = 'https://html5css.ru/howto/howto_js_rangeslider.php' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
-driver_chrome.set_window_size(1920, 1080) # окно разрешения
+driver_chrome.set_window_size(1920, 1080) # задаем параметры окна разрешения
 action = ActionChains(driver_chrome)
 
 # двигаем ползунок Round
-slider = driver_chrome.find_element(By.XPATH, '//input[@class = "slider-color"]')
-action.click_and_hold(slider).move_by_offset(-500, 0).release().perform()
-check_value = driver_chrome.find_element(By.XPATH, "//span[@id = 'f']")
+slider = driver_chrome.find_element(By.XPATH, '//input[@class = "slider-color"]') # ссылка на селект
+time.sleep(1)
+action.click_and_hold(slider).move_by_offset(-450, 0).release().perform() # сдвигаем ползунок до 15
+check_value = driver_chrome.find_element(By.XPATH, "//span[@id = 'f']") # ссылка для проверки значения Value
 check_text = check_value.text
 print(f"Значение value: {check_text}")
-assert check_text == "11"
+assert check_text == "15"
 print('Проверка пройдена')
 
-time.sleep(5)
+time.sleep(3)
 
 driver_chrome.close()
