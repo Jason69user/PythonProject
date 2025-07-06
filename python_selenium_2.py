8539db21d15495bf862a1fde293350c0813289d4
Dimitry <dartahilles@mail.ru>
2025-07-06 17:16:07 +0400

    подключаем Faker

--- python_selenium_2.py
+++ python_selenium_2.py
@@ -1,7 +1,7 @@
 import time
 
 from selenium import webdriver
-
+from faker import Faker
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,19 +12,16 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://lambdatest.com/selenium-playground/jquery-dropdown-search-demo' # Даём ссылку на тестируемый сайт
+base_url = 'https://www.saucedemo.com' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.set_window_size(1920, 1080) # задаем параметры окна разрешения
+fake = Faker('en_US') # инициализируем faker
 
-# нажимаем на dropdown
-select_drop = driver_chrome.find_element(By.XPATH, "//span[@aria-labelledby = 'select2-country-container']")
-select_drop.click()
-
-# выбираем страну из dropdown
-select_country = driver_chrome.find_element(By.XPATH, "(//li[@class = 'select2-results__option'])[8]")
-select_country.click()
-
-time.sleep(3)
+# вставляем в поле Username сгенерированное имя
+name = fake.name()
+user_name = driver_chrome.find_element(By.ID, 'user-name')
+user_name.send_keys(name) # вводим логин
+time.sleep(2)
 
 driver_chrome.close()
