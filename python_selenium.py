50d1d7bf6fcc19428317e5df834e8d7792c0fe1e
Dimitry <dartahilles@mail.ru>
2025-07-08 15:08:28 +0400

    работаем с alert

--- python_selenium.py
+++ python_selenium.py
@@ -12,29 +12,39 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://demoqa.com/browser-windows' # Даём ссылку на тестируемый сайт
+base_url = 'https://the-internet.herokuapp.com/javascript_alerts' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.set_window_size(1261, 2399) # задаем параметры окна разрешения
 
-# открываем вторую вкладку, переключаемся на неё и возвращаемся обратно
-tab_button = driver_chrome.find_element(By.XPATH, "//button[@id = 'tabButton']")
-tab_button.click()
-driver_chrome.switch_to.window(driver_chrome.window_handles[1])
-print("Переключились на вторую вкладку")
-time.sleep(2)
-driver_chrome.switch_to.window(driver_chrome.window_handles[0])
-print("Переключились на первую вкладку")
-time.sleep(2)
+# закрываем окно Alert
+click_alert = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsAlert()']")
+click_alert.click()
+print("Кликаем на Click for JS Alert")
+time.sleep(1)
+driver_chrome.switch_to.alert.accept()
+print("Кликаем Закрыть")
+time.sleep(1)
 
-# открываем второе окно, переключаемся на него и возвращаемся обратно
-window_button = driver_chrome.find_element(By.XPATH, "//button[@id = 'windowButton']")
-window_button.click()
-driver_chrome.switch_to.window(driver_chrome.window_handles[1])
-print("Переключились на второе окно")
-time.sleep(2)
-driver_chrome.switch_to.window(driver_chrome.window_handles[0])
-print("Переключились на первое окно")
-time.sleep(2)
+# закрываем окно Alert кнопкой "нет"
+click_confirm = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsConfirm()']")
+click_confirm.click()
+print("Кликаем на Click for JS Confirm")
+time.sleep(1)
+driver_chrome.switch_to.alert.dismiss()
+print("Кликаем Нет")
+time.sleep(1)
+
+# вводим слово в поле и закрываем окно Alert кнопкой "продолжить"
+click_prompt = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsPrompt()']")
+click_prompt.click()
+print("Кликаем на Click for JS Prompt")
+time.sleep(1)
+driver_chrome.switch_to.alert.send_keys("Ave Maria")
+print("Вводим в поле: 'Ave Maria'")
+time.sleep(1)
+driver_chrome.switch_to.alert.accept()
+print("Кликаем Продолжить")
+time.sleep(1)
 
 driver_chrome.close()
