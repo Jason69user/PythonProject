032bbc1e88687a833801aff1f74d7c59413e0f8c
Dimitry <dartahilles@mail.ru>
2025-07-10 15:55:22 +0400

    загружаем файл в браузер

--- python_selenium.py
+++ python_selenium.py
@@ -1,5 +1,7 @@
+import os
 import time
 
+from unittest.mock import patch
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -12,39 +14,24 @@ options.add_experimental_option("detach", True)
 options.add_argument('--incognito')
 
 driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://the-internet.herokuapp.com/javascript_alerts' # Даём ссылку на тестируемый сайт
+base_url = 'https://www.lambdatest.com/selenium-playground/upload-file-demo' # Даём ссылку на тестируемый сайт
 
 driver_chrome.get(base_url) # открываем ссылку в браузере Chrome
 driver_chrome.set_window_size(1261, 2399) # задаем параметры окна разрешения
 
-# закрываем окно Alert
-click_alert = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsAlert()']")
-click_alert.click()
-print("Кликаем на Click for JS Alert")
-time.sleep(1)
-driver_chrome.switch_to.alert.accept()
-print("Кликаем Закрыть")
+# получаем путь до файла
+patch_pload = "D:\\GitHub\\Project\\PythonProject\\screen\\shaman.jpg"
+
+# загружаем файл в браузер
+click_button = driver_chrome.find_element(By.XPATH, "//input[@id='file']")
+click_button.send_keys(patch_pload)
 time.sleep(1)
 
-# закрываем окно Alert кнопкой "нет"
-click_confirm = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsConfirm()']")
-click_confirm.click()
-print("Кликаем на Click for JS Confirm")
-time.sleep(1)
-driver_chrome.switch_to.alert.dismiss()
-print("Кликаем Нет")
-time.sleep(1)
-
-# вводим слово в поле и закрываем окно Alert кнопкой "продолжить"
-click_prompt = driver_chrome.find_element(By.XPATH, "//button[@onclick= 'jsPrompt()']")
-click_prompt.click()
-print("Кликаем на Click for JS Prompt")
-time.sleep(1)
-driver_chrome.switch_to.alert.send_keys("Ave Maria")
-print("Вводим в поле: 'Ave Maria'")
-time.sleep(1)
-driver_chrome.switch_to.alert.accept()
-print("Кликаем Продолжить")
-time.sleep(1)
+# берем название файла и проверяем на корректность
+value = click_button.get_attribute("value")
+actual_file_name = os.path.basename(value)
+print(actual_file_name)
+assert actual_file_name == "shaman.jpg"
+print("Название файла совпадает")
 
 driver_chrome.close()
