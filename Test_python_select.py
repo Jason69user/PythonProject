2f8d73bdf5f9d889976e8964253e6cd7952d9b74
Dimitry <dartahilles@mail.ru>
2025-07-27 18:39:27 +0400

    перенес опции в init класса

--- Test_python_select.py
+++ Test_python_select.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
 
 # создаем класс
 class Test:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
 
     def test_select_prod(self):
-        driver_chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        driver_chrome = webdriver.Chrome(options=self.options, service=ChromeService(ChromeDriverManager().install()))
         base_url = 'https://www.saucedemo.com'  # Даём ссылку на тестируемый сайт
         driver_chrome.get(base_url)  # открываем ссылку в браузере Chrome
         driver_chrome.set_window_size(1261, 924)  # задаем параметры окна разрешения
